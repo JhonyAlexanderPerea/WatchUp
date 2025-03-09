c8c0e7ff2acde20a1d5fc887f01e6cd66bc95e00
JhonyAlexanderPerea <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2025-03-08 23:29:05 -0500

    Initial commit :)
    Psdt: Saquenme de ingeniería. 💀

---
+++ src/main/java/co/uniquindio/proyectofinalprogramacionavanzada/ProyectoFinalProgramacionAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package co.uniquindio.proyectofinalprogramacionavanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalProgramacionAvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalProgramacionAvanzadaApplication.class, args);
+	}
+
+}
