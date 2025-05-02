72ef6ada0952b174c09e9355249b09fe85c577d3
danie <daniels.perezc@uqvirtual.edu.co>
2025-05-02 01:58:01 -0500

    Borrado de clases inservibles

--- src/main/java/co/uniquindio/service/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package co.uniquindio.service;
-
-import co.uniquindio.dtos.request.LoginRequest;
-import co.uniquindio.dtos.response.TokenResponse;
-
-public interface SecurityService {
-    TokenResponse login(LoginRequest request);
-
-}
