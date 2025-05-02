b84d02e1c83495bf45f637bdaefaecf9bc408e45
danie <daniels.perezc@uqvirtual.edu.co>
2025-05-01 19:59:12 -0500

    Cambio en el SecurityConfig para ver los end points publicos

--- src/main/java/co/uniquindio/config/SecurityConfig.java
+++ src/main/java/co/uniquindio/config/SecurityConfig.java
@@ -1,28 +1,36 @@
 package co.uniquindio.config;
 
 import co.uniquindio.security.JwtAuthFilter;
+import co.uniquindio.security.PublicEndPoints;
 import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private final JwtDecoder jwtDecoder;
+    private final JwtAuthFilter jwtAuthFilter;
+    private final PublicEndPoints publicEndPoints;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+        // version1
 //        http
 //                .csrf(csrf -> csrf.disable())  // Desactiva CSRF
 //                .authorizeHttpRequests(auth -> auth
@@ -33,18 +41,47 @@ public class SecurityConfig {
 //                .httpBasic(basic -> basic.disable());  // Desactiva autenticación básica
 //
 //        return http.build();
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login").permitAll() // Ruta pública
-                        .anyRequest().permitAll() // Todas las demás requieren autenticación
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // 👈 Filtro JWT
 
+        // version 2
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/auth/login").permitAll() // Ruta pública
+//                        .anyRequest().permitAll() // Todas las demás requieren autenticación
+//                )
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // 👈 Filtro JWT
+//
+//        return http.build();
+
+        //version3
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(publicEndPoints.getMatchers()).permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .sessionManagement(
+                        (session) -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder)));
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager
+
+            (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
--- src/main/java/co/uniquindio/security/PublicEndPoints.java
+++ src/main/java/co/uniquindio/security/PublicEndPoints.java
@@ -1,15 +1,19 @@
 package co.uniquindio.security;
 
 import lombok.Getter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
 public class PublicEndPoints {
-    private final String[] PUBLIC_URLS = {
-            "/api/v1/auth/**",
-            "/api/v1/users/**",
-            "/api/v1/categories/**",
-            "/api/v1/notifications/**"
+    private final RequestMatcher[] matchers = new RequestMatcher[] {
+            new AntPathRequestMatcher("/auth/**"),
+            new AntPathRequestMatcher("/reports", HttpMethod.GET.name()),
+            new AntPathRequestMatcher("/reports/*", HttpMethod.GET.name()),
+            new AntPathRequestMatcher("/categories", HttpMethod.GET.name()),
+            new AntPathRequestMatcher("/categories/*", HttpMethod.GET.name())
     };
 }
