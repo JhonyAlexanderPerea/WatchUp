b9c4759ee8e96cea655b9488300be2eb0035b0dc
Mbappe <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2025-05-18 16:57:40 -0500

    update

--- src/main/java/co/uniquindio/ProyectoFinalProgramacionAvanzadaApplication.java
+++ src/main/java/co/uniquindio/ProyectoFinalProgramacionAvanzadaApplication.java
@@ -11,9 +11,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class ProyectoFinalProgramacionAvanzadaApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		System.setProperty("MONGO_DB_USERNAME", dotenv.get("MONGO_DB_USERNAME"));
-		System.setProperty("MONGO_DB_PASSWORD", dotenv.get("MONGO_DB_PASSWORD"));
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(ProyectoFinalProgramacionAvanzadaApplication.class, args);
 	}
 
