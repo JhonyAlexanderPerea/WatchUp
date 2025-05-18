f8630706fe15c7c7ff8a0898ca38e7de25ab5947
Mbappe <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2025-05-18 01:07:30 -0500

    Despliegue de la BD

--- src/main/java/co/uniquindio/ProyectoFinalProgramacionAvanzadaApplication.java
+++ src/main/java/co/uniquindio/ProyectoFinalProgramacionAvanzadaApplication.java
@@ -1,15 +1,20 @@
 package co.uniquindio;
 
 import co.uniquindio.setup.DefaultUserProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties(DefaultUserProperties.class)
 @SpringBootApplication //(scanBasePackages = "co.uniquindio")
+
 public class ProyectoFinalProgramacionAvanzadaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("MONGO_DB_USERNAME", dotenv.get("MONGO_DB_USERNAME"));
+		System.setProperty("MONGO_DB_PASSWORD", dotenv.get("MONGO_DB_PASSWORD"));
 		SpringApplication.run(ProyectoFinalProgramacionAvanzadaApplication.class, args);
 	}
 
